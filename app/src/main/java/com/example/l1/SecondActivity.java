09b240a19a8bb1ab6e1123d0730b74e4859348d1
FedorBGR <fedor.korolev.2004@mail.ru>
2024-10-06 21:41:42 +0300

    Лаба 1 рабочая

---
+++ app/src/main/java/com/example/l1/SecondActivity.java
@@ -0,0 +1,20 @@
+package com.example.l1;
+
+import android.os.Bundle;
+import android.widget.TextView;
+import androidx.appcompat.app.AppCompatActivity;
+
+public class SecondActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity2);
+
+
+        String surname = getIntent().getStringExtra("surname_key");
+        
+        TextView textView = findViewById(R.id.textView);
+        textView.setText("Переданный параметр: " + surname);
+    }
+}
