bc199bed0480f403e8599fba2e02d49c8cf245f1
FedorBGR <fedor.korolev.2004@mail.ru>
2024-10-09 13:24:23 +0300

    Лаба 1 рабочая еще раз

--- app/src/main/java/com/example/l1/MainActivity.java
+++ app/src/main/java/com/example/l1/MainActivity.java
@@ -8,14 +8,13 @@ import androidx.appcompat.app.AppCompatActivity;
 
 public class MainActivity extends AppCompatActivity {
 
-    @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
         Button btn1 = findViewById(R.id.btn1);
         btn1.setOnClickListener(new View.OnClickListener() {
-            @Override
+
             public void onClick(View v) {
 
                 String surname = "Королев";
--- app/src/main/java/com/example/l1/SecondActivity.java
+++ app/src/main/java/com/example/l1/SecondActivity.java
@@ -6,7 +6,6 @@ import androidx.appcompat.app.AppCompatActivity;
 
 public class SecondActivity extends AppCompatActivity {
 
-    @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity2);
